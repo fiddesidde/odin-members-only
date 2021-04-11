d247d17bdb20681ae2158c14f324213717eeb1c3
fiddesidde <fredrik.mellberg@gmail.com>
2021-04-11 21:42:07 +0200

    edit default port

--- app.js
+++ app.js
@@ -119,5 +119,5 @@ app.use((err, req, res, next) => {
   res.status(statusCode).render('error', { err });
 });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3050;
 app.listen(port, () => console.log(`app listening on port ${port}!`));
